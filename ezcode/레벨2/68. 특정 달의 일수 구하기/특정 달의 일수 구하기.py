8f982ff066c53a359e82842219adb26ebe421364
Taeik Kim <thezz9@naver.com>
2025-07-07 00:13:45 +0900

    [레벨2] 문제 68. 특정 달의 일수 구하기, 메모리: 3966KB, 시간: 20ms

--- ezcode/레벨2/68. 특정 달의 일수 구하기/특정 달의 일수 구하기.py
+++ ezcode/레벨2/68. 특정 달의 일수 구하기/특정 달의 일수 구하기.py
@@ -1,10 +1,11 @@
-y, m = map(int, input().split())  # y: year, m: month
+year, month = map(int, input().split())
 
+# 각 달의 일 수 (인덱스 0은 사용하지 않음)
 days = [0, 31, 28, 31, 30, 31, 30,
         31, 31, 30, 31, 30, 31]
 
-# 윤년이면 2월을 29일로
-if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+# 윤년이면 2월을 29일로 설정
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     days[2] = 29
 
-print(days[m])
+print(days[month])
