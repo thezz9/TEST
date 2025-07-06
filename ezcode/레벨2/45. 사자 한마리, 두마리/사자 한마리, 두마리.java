15295677d475fff8cc0ffa2b10a585a60f805d7b
Taeik Kim <thezz9@naver.com>
2025-07-07 00:37:39 +0900

    [레벨2] 문제 45. 사자 한마리, 두마리, 메모리: 12557KB, 시간: 158ms

--- ezcode/레벨2/45. 사자 한마리, 두마리/사자 한마리, 두마리.java
+++ ezcode/레벨2/45. 사자 한마리, 두마리/사자 한마리, 두마리.java
@@ -4,7 +4,7 @@ public class Main {
     static int H, W;
     static char[][] grid;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0}; // 상하좌우 이동
+    static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) {
