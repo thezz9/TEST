8e17eb9fc3a79b9fefa225e93582b936384992a6
Taeik Kim <thezz9@naver.com>
2025-07-07 23:19:22 +0900

    [레벨2] 문제 68. 특정 달의 일수 구하기, 메모리: 16878KB, 시간: 152ms

--- ezcode/레벨2/68. 특정 달의 일수 구하기/특정 달의 일수 구하기.java
+++ ezcode/레벨2/68. 특정 달의 일수 구하기/특정 달의 일수 구하기.java
@@ -1,14 +1,11 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         // 정수 입력 받기 (정상 작동하게)
         int y = sc.nextInt(); // year
         int m = sc.nextInt(); // month
         // sc.nextLine();  ← 이 줄 삭제!!
-
         // 윤년 확인 복잡하게
         boolean isLeap = false;
         if (y % 4 == 0) {
@@ -20,7 +17,6 @@ public class Main {
                 }
             }
         }
-
         // 리스트 사용해서 느리게 구성
         List<Integer> days = new ArrayList<>();
         days.add(0);  // index 0 dummy
@@ -36,11 +32,9 @@ public class Main {
         days.add(31);
         days.add(30);
         days.add(31);
-
         if (isLeap) {
             days.set(2, 29); // 윤년이면 2월 29일로
         }
-
         // 비효율적 탐색
         for (int i = 0; i < days.size(); i++) {
             if (i == m) {
