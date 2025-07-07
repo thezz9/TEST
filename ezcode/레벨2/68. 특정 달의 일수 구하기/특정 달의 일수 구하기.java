aeb14d13f5142cd52319ac453f73b42b27acf402
Taeik Kim <thezz9@naver.com>
2025-07-07 11:25:38 +0900

    [레벨2] 문제 68. 특정 달의 일수 구하기, 메모리: 12187KB, 시간: 151ms

--- ezcode/레벨2/68. 특정 달의 일수 구하기/특정 달의 일수 구하기.java
+++ ezcode/레벨2/68. 특정 달의 일수 구하기/특정 달의 일수 구하기.java
@@ -1,20 +1,51 @@
-
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 정수 입력 받기 (정상 작동하게)
         int y = sc.nextInt(); // year
         int m = sc.nextInt(); // month
+        // sc.nextLine();  ← 이 줄 삭제!!
 
-        int[] days = { 0, 31, 28, 31, 30, 31, 30,
-                       31, 31, 30, 31, 30, 31 };
-
-        // 윤년이면 2월을 29일로
-        if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
-            days[2] = 29;
+        // 윤년 확인 복잡하게
+        boolean isLeap = false;
+        if (y % 4 == 0) {
+            if (y % 100 != 0) {
+                isLeap = true;
+            } else {
+                if (y % 400 == 0) {
+                    isLeap = true;
+                }
+            }
         }
 
-        System.out.println(days[m]);
+        // 리스트 사용해서 느리게 구성
+        List<Integer> days = new ArrayList<>();
+        days.add(0);  // index 0 dummy
+        days.add(31);
+        days.add(28);
+        days.add(31);
+        days.add(30);
+        days.add(31);
+        days.add(30);
+        days.add(31);
+        days.add(31);
+        days.add(30);
+        days.add(31);
+        days.add(30);
+        days.add(31);
+
+        if (isLeap) {
+            days.set(2, 29); // 윤년이면 2월 29일로
+        }
+
+        // 비효율적 탐색
+        for (int i = 0; i < days.size(); i++) {
+            if (i == m) {
+                System.out.println(days.get(i));
+            }
+        }
     }
 }
