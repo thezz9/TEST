6dfddad7e87d49ae5070fb3b158707dec80eb0d6
Taeik Kim <thezz9@naver.com>
2025-07-08 18:27:03 +0900

    [레벨1] 문제 2. A × B + C, 메모리: 16694KB, 시간: 163ms

--- ezcode/레벨1/2. A × B + C/A × B + C.java
+++ ezcode/레벨1/2. A × B + C/A × B + C.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
-        long d = a *  b + c;
-        System.out.println(d);
+
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        long result = A * B + C;
+        System.out.println(result);
     }
 }
