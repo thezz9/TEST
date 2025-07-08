978051d0514f94e7dc32113b7a6ba75d97abb959
Taeik Kim <thezz9@naver.com>
2025-07-08 18:21:24 +0900

    [레벨1] 문제 2. A × B + C, 메모리: 16778KB, 시간: 157ms

--- ezcode/레벨1/2. A × B + C/A × B + C.java
+++ ezcode/레벨1/2. A × B + C/A × B + C.java
@@ -1,19 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] tokens = br.readLine().split(" ");
-        
-        BigInteger A = new BigInteger(tokens[0]);
-        BigInteger B = new BigInteger(tokens[1]);
-        BigInteger C = new BigInteger(tokens[2]);
-        
-        // A × B + C 계산
-        BigInteger result = A.multiply(B).add(C);
-        System.out.println(result);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        long d = a *  b + c;
+        System.out.println(d);
     }
 }
